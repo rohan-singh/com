95f1b4811f076da1eddfc193aa21dd2d46dabfd5
Rohan Singh <rohan.vishen@gmail.com>
2018-05-06 13:31:29 +0530

    Create Library.cpp

---
+++ prototype/Library.cpp
@@ -0,0 +1,93 @@
+#include "Library.h"
+//#include<iostream>
+
+#include <windows.h> // for OutputDebugString
+#define TRACE OutputDebugString
+
+struct Hen : IHen2 , IOfflineChicken
+{
+	unsigned m_count;
+
+	//ctor
+	Hen() : m_count(0)
+	{
+		TRACE(L"Cheep!\n");
+	}
+
+	//dtor
+	~Hen()
+	{
+		TRACE(L"Chicken Soup!\n");
+	}
+
+	//
+	// IObject
+	//
+	void _stdcall AddRef()
+	{
+		++m_count;
+	}
+
+	void _stdcall Release()
+	{
+		if (0 == --m_count)
+		{
+			delete this;
+		}
+	}
+
+	void* _stdcall As(char const* type)
+	{
+		if (0 == strcmp(type, "IHen2") ||
+			0 == strcmp(type, "IHen") ||
+			0 == strcmp(type, "IObject"))
+		{
+			AddRef();
+			return static_cast<IHen2*>(this);
+		}
+		else if(0 == strcmp(type, "IOfflineChicken"))
+		{
+			AddRef();
+			return static_cast<IOfflineChicken*>(this);
+		}
+		else
+		{
+			return 0; // basically null
+		}
+	}
+
+	//
+	// IHen
+	//
+	void _stdcall Cluck()
+	{
+		TRACE(L"Cluck\n");
+	}
+
+	void _stdcall Roost()
+	{
+		TRACE(L"Roost\n");
+	}
+
+	//
+	// IHen2
+	//
+	void _stdcall Forage()
+	{
+		TRACE(L"Forage!\n");
+	}
+
+	//
+	// IOfflineChicken
+	//
+	void _stdcall Load(char const* /*file*/)
+	{
+		TRACE(L"Cluck\n");
+	}
+
+	void _stdcall Save(char const* /*file*/)
+	{
+		TRACE(L"Roost\n");
+	}
+
+};
